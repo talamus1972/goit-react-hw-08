57006a0e06cbaf5ba618e0cf3aac91c12be8242c
talamus1972 <talamus1972@gmail.com>
2024-03-31 01:20:43 +0200

    9

--- src/components/pages/LoginPage/LoginPage.jsx
+++ src/components/pages/LoginPage/LoginPage.jsx
@@ -3,17 +3,21 @@ import Loader from "../../Loader/Loader.jsx";
 import LoginForm from "../../LoginForm/LoginForm.jsx";
 import { selectError, selectLoading } from "../../../redux/auth/selectors.js";
 import ErrorMessage from "../../ErrorMessage/ErrorMessage.jsx";
+import { Link } from "react-router-dom";
 
 export default function LoginPage() {
   const isLoading = useSelector(selectLoading);
-  const error = useSelector(selectError)
+  const error = useSelector(selectError);
 
   return (
     <div>
       <h1>Log in</h1>
       <LoginForm />
-     {isLoading && <Loader />}
+      {isLoading && <Loader />}
       {error && <ErrorMessage />}
+      <p>
+        or <Link to="/register">register</Link>
+      </p>
     </div>
   );
 }
