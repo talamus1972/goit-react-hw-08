aa3a7fb370932d9728cfcea712cedf0a0d9c9f10
talamus1972 <talamus1972@gmail.com>
2024-04-01 02:44:06 +0300

    f

--- src/components/ConfirmationModal/ConfirmationModal.jsx
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,6 +1,6 @@
-import css from "./ConfirmationModal.module.css"
+import css from "./ConfirmationModal.module.css";
 
-export default function ConfirmationModal ({ isOpen, onClose, onConfirm })  {
+export default function ConfirmationModal({ isOpen, onClose, onConfirm }) {
   if (!isOpen) return null;
 
   const handleConfirm = () => {
@@ -11,11 +11,18 @@ export default function ConfirmationModal ({ isOpen, onClose, onConfirm })  {
   return (
     <div className={css.modal}>
       <div className={css.modalContent}>
-        <p className={css.text}>Are you sure you want to delete this contact?</p>
-        <button className={css.button} onClick={handleConfirm}>Confirm</button>
-        <button className={css.button} onClick={onClose}>Cancel</button>
+        <p className={css.text}>
+          Are you sure you want to delete this contact?
+        </p>
+        <div className={css.btn}>
+          <button className={css.button} onClick={handleConfirm}>
+            Confirm
+          </button>
+          <button className={css.button} onClick={onClose}>
+            Cancel
+          </button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
-
